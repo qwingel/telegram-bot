af598c5b3523f71f1cda26a602a1df4c631b3c0d
qwingel <svatgdanov@gmail.com>
2023-05-12 02:41:32 +0300

    first commit

---
+++ database.py
@@ -0,0 +1,22 @@
+import sqlite3
+import calendar
+from datetime import date
+
+db = sqlite3.connect('database.db', 10.0, check_same_thread=False)
+
+def get_user_lessons(day: str):
+    user_class = '10б'
+    # req = 'SELECT * FROM ' + day + ' ' + 'WHERE class = ' + user_class
+    # print(req)
+    if day == 'Monday': res = db.execute("""SELECT * FROM Monday WHERE class = ?""", (user_class, ))
+    if day == 'Tuesday': res = db.execute("""SELECT * FROM Tuesday WHERE class = ?""", (user_class, ))
+    if day == 'Wednesday': res = db.execute("""SELECT * FROM Wednesday WHERE class = ?""", (user_class, ))
+    if day == 'Thursday': res = db.execute("""SELECT * FROM Thursday WHERE class = ?""", (user_class, ))
+    if day == 'Friday': res = db.execute("""SELECT * FROM Friday WHERE class = ?""", (user_class, ))
+    if day == 'Saturday': res = db.execute("""SELECT * FROM Saturday WHERE class = ?""", (user_class, ))
+    lessons = res.fetchone()
+    
+    if lessons is None:
+        return None
+    
+    return lessons
---
+++ main.py
@@ -0,0 +1,35 @@
+import telebot
+import calendar
+from database import get_user_lessons
+from datetime import date
+from datetime import time
+
+token = telebot.TeleBot('6232931993:AAG-fax5xiFD0HSiNn59S2C8Z1pSr9hGLx0')
+
+thisDate = str(date.today()).split('-')
+today = int(calendar.weekday(int(thisDate[0]), int(thisDate[1]), int(thisDate[2])))
+current_time = str(time.hour) + str(time.minute) + str(time.second)
+
+dayOfWeek = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
+rus_dayOfWeek = ("Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота")
+
+@token.message_handler(commands=['today'])
+def today_timeTable(message):
+    mess = '' 
+    for i in range(1, len(get_user_lessons(dayOfWeek[today]))):
+        if not get_user_lessons(dayOfWeek[today])[i]:
+            break
+        mess += '-' + get_user_lessons(dayOfWeek[today])[i] + '\n'
+    token.send_message(message.chat.id, f'<b>[{rus_dayOfWeek[today]}]</b> \n\n' + mess + f'\n<b>[{rus_dayOfWeek[today]}]</b>', parse_mode='html')
+
+@token.message_handler(commands=['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'])
+def timeTable(message):
+    mess=''
+    for i in range(1, len(get_user_lessons(message.text[1:].title()))):
+        if not get_user_lessons(message.text[1:].title())[i]:
+            break
+        mess += '-' + get_user_lessons(message.text[1:].title())[i] + '\n'
+    token.send_message(message.chat.id, f'<b>[{rus_dayOfWeek[dayOfWeek.index(message.text[1:].title())]}]</b> \n\n' + mess + f'\n<b>[{rus_dayOfWeek[dayOfWeek.index(message.text[1:].title())]}]</b>', parse_mode='html')
+
+if __name__ == '__main__':
+    token.polling(none_stop=True)
